b2ee67cf4aef70e370d77354f578d5db1bac2ef8
Roberto Angulo Torrealba <Roberto.Angulo@endava.com>
2020-08-22 19:10:40 -0500

    add logic to form

--- src/components/forms/ContactForm.jsx
+++ src/components/forms/ContactForm.jsx
@@ -7,43 +7,85 @@ const { contactText, buttonText } = {
   buttonText: "Enviar",
 }
 
-const ContactForm = () => (
-  <form method="POST" name="contactForm" data-netlify="true" action="/blog">
-    <input type="hidden" name="form-name" value="contact" />
-    <h4 className="mb-3 font-weight-light text-white">{contactText}</h4>
-    <p>
-      <input
-        type="text"
-        id="nameInput"
-        className="form-control"
-        placeholder="Nombre"
-        name="nameInput"
-      />
-    </p>
+const ContactForm = () => {
+  const [formState, setFormState] = React.useState({
+    name: "",
+    email: "",
+    message: "",
+  })
 
-    <p>
-      <input
-        type="email"
-        id="emailInput"
-        className="form-control"
-        placeholder="Email"
-        name="emailInput"
-      />
-    </p>
+  const { name, email, message } = formState
 
-    <p>
-      <textarea
-        placeholder="Mensaje"
-        name="textareaInput"
-        id="textareaInput"
-        className="form-control"
-        rows="3"
-      ></textarea>
-    </p>
+  const encode = data => {
+    return Object.keys(data)
+      .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+      .join("&")
+  }
 
-    <button type="submit" className={cx("btn", CSS.ButtonContact)}>
-      {buttonText}
-    </button>
-  </form>
-)
+  const handleSubmit = () => {
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "contactForm", ...formState }),
+    })
+      .then(() => alert("Success!"))
+      .catch(error => alert(error))
+
+    e.preventDefault()
+  }
+
+  const handleChange = e => setFormState({ [e.target.name]: e.target.value })
+
+  return (
+    <form
+      method="POST"
+      name="contactForm"
+      data-netlify="true"
+      action="/blog"
+      onSubmit={handleSubmit}
+    >
+      <input type="hidden" name="form-name" value="contactForm" />
+      <h4 className="mb-3 font-weight-light text-white">{contactText}</h4>
+      <p>
+        <input
+          type="text"
+          id="nameInput"
+          className="form-control"
+          placeholder="Nombre"
+          name="nameInput"
+          value={name}
+          onChange={handleChange}
+        />
+      </p>
+
+      <p>
+        <input
+          type="email"
+          id="emailInput"
+          className="form-control"
+          placeholder="Email"
+          name="emailInput"
+          value={email}
+          onChange={handleChange}
+        />
+      </p>
+
+      <p>
+        <textarea
+          placeholder="Mensaje"
+          name="textareaInput"
+          id="textareaInput"
+          className="form-control"
+          value={message}
+          onChange={handleChange}
+          rows="3"
+        ></textarea>
+      </p>
+
+      <button type="submit" className={cx("btn", CSS.ButtonContact)}>
+        {buttonText}
+      </button>
+    </form>
+  )
+}
 export default ContactForm
